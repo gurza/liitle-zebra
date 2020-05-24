45c3af3a76ff311bf34f3fb21420df89639c4e8c
Sergey Popinevskiy <gurza000@gmail.com>
2020-05-24 10:45:31 +0300

    Implement Telegram bot skeleton

--- bot.py
+++ bot.py
@@ -1,2 +1,17 @@
+# -*- coding: utf-8 -*-
+import os
+
+import telebot
+
+token = os.getenv("ZEBRA_TOKEN", "")
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=["start"])
+def start(message: telebot.types.Message):
+    start_message = "I am Zebra, barcode scanner. Send me image with barcode."
+    bot.send_message(message.chat.id, start_message)
+
+
 if __name__ == "__main__":
-    print("I am zebra")
+    bot.polling()
