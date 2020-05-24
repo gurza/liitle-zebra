57d9882753646ca3ab403667821c413e0fd07bb8
Sergey Popinevskiy <gurza000@gmail.com>
2020-05-24 12:30:46 +0300

    Implement barcode scanner

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
+import io
+from PIL import Image
 
 from aiogram import Bot, Dispatcher, executor, types
+import pyzbar.pyzbar as pyzbar
 
 token = os.getenv("ZEBRA_TOKEN", "")
 bot = Bot(token)
@@ -12,6 +15,21 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     start_message = "I am Zebra, barcode scanner. Send me image with barcode."
     await message.answer(start_message)
+
+
+@dp.message_handler(content_types=[types.ContentType.PHOTO])
+async def scan(message: types.Message):
+    image_data = await bot.download_file_by_id(message.photo[0].file_id)
+    image = Image.open(io.BytesIO(image_data.getvalue()))
+    decoded_objects = pyzbar.decode(image)
+
+    result_message = "Couldn't find the barcode"
+    if len(decoded_objects):
+        result_message = ",".join([
+            "{data} ({type})".format(data=obj.data.decode("utf-8"), type=obj.type)
+            for obj in decoded_objects
+        ])
+    await message.reply(result_message)
 
 
 if __name__ == "__main__":
